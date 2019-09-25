c7acc8c25f4ed7da2b54add1d2a8dd37a0b82573
mirkowi <mirko.wittek@gmail.com>
2019-09-25 18:45:57 +0200

    Fehlerbehebung bei minimaler Zykluszeit

--- PlcTask.cpp
+++ PlcTask.cpp
@@ -15,10 +15,10 @@ void TPlcTask::run() {
     gettimeofday(&now,0);
     // Aus dem Zeitvergleich Differenz in Mikrosekunden ausrechnen
     long usDiff = (now.tv_sec - lastTime.tv_sec)*1000000 + (now.tv_usec - lastTime.tv_usec);
-    if (lastTime.tv_sec == 0 || usDiff/1000000 >= minCycleSetMs) {
+    if (lastTime.tv_sec == 0 || usDiff/1000 >= minCycleSetMs) {
         if (lastTime.tv_sec!=0) {
             // Zykluszeit messen und min/max setzen
-            cycleActMs = usDiff/1000000;
+            cycleActMs = usDiff/1000;
             if (minCycleActMs==0 || minCycleActMs>cycleActMs) minCycleActMs=cycleActMs;
             if (maxCycleActMs<cycleActMs) maxCycleActMs=cycleActMs;
         }
